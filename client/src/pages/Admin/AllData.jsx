49a8a9f1aa3c37a724abe20569ff74def38710c8
AanBahudin <aan.bahudin11@gmail.com>
2025-01-19 20:32:13 +0800

    finish adding filter function in FE

--- client/src/pages/Admin/AllData.jsx
+++ client/src/pages/Admin/AllData.jsx
@@ -1,43 +1,56 @@
-import { Search, IdCard, CircleUser, ChevronLeft, ChevronRight } from 'lucide-react'
-import { TableData } from '../../components'
-import customFetch from '../../utils/customFetch'
-import { toast } from 'react-toastify'
-import { Link, useLoaderData, useNavigate } from 'react-router'
-import { useState } from 'react'
+import {
+  Search,
+  IdCard,
+  CircleUser,
+  ChevronLeft,
+  ChevronRight,
+} from "lucide-react";
+import { TableData } from "../../components";
+import customFetch from "../../utils/customFetch";
+import { toast } from "react-toastify";
+import {
+  Link,
+  useLoaderData,
+  useNavigate,
+  useSearchParams,
+} from "react-router";
+import { useState } from "react";
 
-export const loader = async({ request }) => {
+export const loader = async ({ request }) => {
   const url = new URL(request.url);
-  const page = url.searchParams.get("page");
-  
+
+  // Ambil semua query params
+  const params = url.searchParams.toString();
   try {
-    const { data } = await customFetch.get(`/medis?page=${page}`)
-    return data
+    const { data } = await customFetch.get(`/medis?${params}`);
+    return data;
   } catch (error) {
-    toast.error(error.response.data.msg)
-    return error
+    toast.error(error.response.data.msg);
+    return error;
   }
-}
+};
 
 const AllData = () => {
-
-  const data = useLoaderData()
-  const navigate = useNavigate()
+  const data = useLoaderData();
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
   const [filter, setFilter] = useState({
-    _id: '',
-    nik: '',
-    nama: '',
-    jenisKelamin: '',
-    tujuanBerobat: '',
-    status: '',
-    kunjungan: '',
-    golonganDarah: ''
-  })
+    _id: "",
+    nik: "",
+    nama: "",
+    jenisKelamin: "Semua",
+    tujuanBerobat: "Semua",
+    status: "Semua",
+    kunjungan: "",
+    golonganDarah: "Semua",
+  });
 
   const handleFilterChange = (event, filterName) => {
     setFilter((prev) => ({
-      ...prev, [filterName]: event.target.value
-    }))
-  }
+      ...prev,
+      [filterName]: event.target.value,
+    }));
+  };
 
   const handleSearch = () => {
     const queryParams = new URLSearchParams();
@@ -53,147 +66,252 @@ const AllData = () => {
     navigate(`?${queryParams.toString()}`);
   };
 
+  const handlePageChange = (newPage) => {
+    // Salin semua query parameters yang ada
+    const params = new URLSearchParams(searchParams);
+
+    // Perbarui atau tambahkan parameter `page`
+    params.set("page", newPage);
+
+    // Arahkan ke URL baru dengan query parameters yang diperbarui
+    navigate(`?${params.toString()}`);
+  };
+
   return (
     <section className="w-full flex flex-col items-start justify-center pb-20">
-      
-      <h1 className='text-xl mb-2 font-medium'>Pencarian Rekam Medis</h1>
-      <article className='w-full bg-white rounded-lg shadow-lg px-4 py-6'>
-
-        <div className='w-full flex gap-x-4 items-center justify-end'>
-          
-          <div className='w-[40%] flex flex-col gap-y-1'>
-            <label htmlFor="" className='text-[14px] font-medium'>rekam medis</label>
-            <div className='flex bg-lightGrey px-2 w-full rounded-md items-center'>
+      <h1 className="text-xl mb-2 font-medium">Pencarian Rekam Medis</h1>
+      <article className="w-full bg-white rounded-lg shadow-lg px-4 py-6">
+        <div className="w-full flex gap-x-4 items-center justify-end">
+          <div className="w-[40%] flex flex-col gap-y-1">
+            <label htmlFor="" className="text-[14px] font-medium">
+              rekam medis
+            </label>
+            <div className="flex bg-lightGrey px-2 w-full rounded-md items-center">
               <Search size={20} />
-              <input onChange={(event) => handleFilterChange(event, '_id')} type="text" placeholder='468749' className={`px-2 py-3 bg-lightGrey ml-2 rounded-sm placeholder:text-[12px] flex-1 outline-none text-[12px]`} />
+              <input
+                onChange={(event) => handleFilterChange(event, "_id")}
+                value={filter._id}
+                type="text"
+                placeholder="468749"
+                className={`px-2 py-3 bg-lightGrey ml-2 rounded-sm placeholder:text-[12px] flex-1 outline-none text-[12px]`}
+              />
             </div>
           </div>
 
-          <div className='w-[25%] flex flex-col gap-y-1'>
-            <label htmlFor="" className='text-[14px] font-medium'>NIK</label>
-            <div className='flex bg-lightGrey px-2 w-full rounded-md items-center'>
+          <div className="w-[25%] flex flex-col gap-y-1">
+            <label htmlFor="" className="text-[14px] font-medium">
+              NIK
+            </label>
+            <div className="flex bg-lightGrey px-2 w-full rounded-md items-center">
               <IdCard size={20} />
-              <input onChange={(event) => handleFilterChange(event, 'nik')} type="text" placeholder='nomor induk keluarga' className={`px-2 py-3 bg-lightGrey ml-2 rounded-sm placeholder:text-[12px] flex-1 outline-none text-[12px]`} />
+              <input
+                onChange={(event) => handleFilterChange(event, "nik")}
+                value={filter.nik}
+                type="text"
+                placeholder="nomor induk keluarga"
+                className={`px-2 py-3 bg-lightGrey ml-2 rounded-sm placeholder:text-[12px] flex-1 outline-none text-[12px]`}
+              />
             </div>
           </div>
 
-          <div className='w-[25%] flex flex-col gap-y-1'>
-            <label htmlFor="" className='text-[14px] font-medium'>nama</label>
-            <div className='flex bg-lightGrey px-2 w-full rounded-md items-center'>
-              <CircleUser className='stroke-greyPrimary' size={20} />
-              <input onChange={(event) => handleFilterChange(event, 'nama')} type="text" placeholder='nama pasien' className={`px-2 py-3 bg-lightGrey ml-2 rounded-sm placeholder:text-[12px] flex-1 outline-none text-[12px]`} />
+          <div className="w-[25%] flex flex-col gap-y-1">
+            <label htmlFor="" className="text-[14px] font-medium">
+              nama
+            </label>
+            <div className="flex bg-lightGrey px-2 w-full rounded-md items-center">
+              <CircleUser className="stroke-greyPrimary" size={20} />
+              <input
+                onChange={(event) => handleFilterChange(event, "nama")}
+                value={filter.nama}
+                type="text"
+                placeholder="nama pasien"
+                className={`px-2 py-3 bg-lightGrey ml-2 rounded-sm placeholder:text-[12px] flex-1 outline-none text-[12px]`}
+              />
             </div>
           </div>
 
-
-          <div className='w-[10%] flex flex-col gap-y-1'>
-            <label htmlFor="" className='text-[14px] font-medium invisible'>Cari data</label>
-            <button onClick={handleSearch} type="submit" className='text-center text-white text-sm px-2 py-3 rounded-md cursor-default bg-blue-500'>Search</button>
+          <div className="w-[10%] flex flex-col gap-y-1">
+            <label htmlFor="" className="text-[14px] font-medium invisible">
+              Cari data
+            </label>
+            <button
+              onClick={handleSearch}
+              type="submit"
+              className="text-center text-white text-sm px-2 py-3 rounded-md cursor-default bg-blue-500"
+            >
+              Search
+            </button>
           </div>
-
         </div>
 
         <div className="w-full flex gap-x-4 items-center justify-between mt-6">
-          <div className='w-[20%] flex flex-col gap-y-1 rounded-lg'>
-            <label htmlFor="" className='text-[14px] font-medium'>Gender</label>
-            <select onChange={(event) => handleFilterChange(event, 'jenisKelamin')} name="" id="" className={`px-2 py-3 bg-lightGrey rounded-sm placeholder:text-[12px] flex-1 outline-none text-[12px]`}>
-              <option value="">All</option>
-              <option value="">Pria</option>
-              <option value="">Wanita</option>
+          <div className="w-[20%] flex flex-col gap-y-1 rounded-lg">
+            <label htmlFor="" className="text-[14px] font-medium">
+              Gender
+            </label>
+            <select
+              onChange={(event) => handleFilterChange(event, "jenisKelamin")}
+              value={filter.jenisKelamin}
+              name=""
+              id=""
+              className={`px-2 py-3 bg-lightGrey rounded-sm placeholder:text-[12px] flex-1 outline-none text-[12px]`}
+            >
+              <option value="">Semua</option>
+              <option value="Pria">Pria</option>
+              <option value="Wanita">Wanita</option>
             </select>
           </div>
 
-          <div className='w-[20%] flex flex-col gap-y-1'>
-            <label htmlFor="" className='text-[14px] font-medium'>Tujuan</label>
-            <div className='flex bg-lightGrey w-full rounded-md items-center'>
-              <select onChange={(event) => handleFilterChange(event, 'tujuanBerobat')} name="" id="" className={`px-2 py-3 bg-lightGrey rounded-sm placeholder:text-[12px] flex-1 outline-none text-[12px]`}>
-                <option value="">All</option>
-                <option value="">Berobat</option>
-                <option value="">Konsultasi</option>
-                <option value="">Kontrol</option>
-                <option value="">Rawat Inap</option>
+          <div className="w-[20%] flex flex-col gap-y-1">
+            <label htmlFor="" className="text-[14px] font-medium">
+              Tujuan
+            </label>
+            <div className="flex bg-lightGrey w-full rounded-md items-center">
+              <select
+                onChange={(event) => handleFilterChange(event, "tujuanBerobat")}
+                value={filter.tujuanBerobat}
+                name=""
+                id=""
+                className={`px-2 py-3 bg-lightGrey rounded-sm placeholder:text-[12px] flex-1 outline-none text-[12px]`}
+              >
+                <option value="">Semua</option>
+                <option value="Berobat">Berobat</option>
+                <option value="Konsultasi">Konsultasi</option>
+                <option value="Kontrol">Kontrol</option>
+                <option value="Rawat Inap">Rawat Inap</option>
               </select>
-            </div>  
+            </div>
           </div>
 
-          <div className='w-[20%] flex flex-col gap-y-1'>
-            <label htmlFor="" className='text-[14px] font-medium'>Status</label>
-              <select onChange={(event) => handleFilterChange(event, 'status')} name="" id="" className={`px-2 py-3 bg-lightGrey rounded-sm placeholder:text-[12px] flex-1 outline-none text-[12px]`}>
-                <option value="">All</option>
-                <option value="">Ditangani</option>
-                <option value="">Proses</option>
-                <option value="">Belum Ditangani</option>
-              </select>
+          <div className="w-[20%] flex flex-col gap-y-1">
+            <label htmlFor="" className="text-[14px] font-medium">
+              Status
+            </label>
+            <select
+              onChange={(event) => handleFilterChange(event, "status")}
+              value={filter.status}
+              name=""
+              id=""
+              className={`px-2 py-3 bg-lightGrey rounded-sm placeholder:text-[12px] flex-1 outline-none text-[12px]`}
+            >
+              <option value="">Semua</option>
+              <option value="Ditangani">Ditangani</option>
+              <option value="Proses">Proses</option>
+              <option value="Belum Ditangani">Belum Ditangani</option>
+            </select>
           </div>
 
-          <div className='w-[20%] flex flex-col gap-y-1'>
-            <label htmlFor="" className='text-[14px] font-medium'>Kunjungan Terakhir</label>
-            <input onChange={(event) => handleFilterChange(event, 'kunjungan')} type='date' name="" id=""   className={`px-2 py-3 bg-lightGrey rounded-sm placeholder:text-[12px] flex-1 outline-none text-[12px]`} />
+          <div className="w-[20%] flex flex-col gap-y-1">
+            <label htmlFor="" className="text-[14px] font-medium">
+              Kunjungan Terakhir
+            </label>
+            <input
+              onChange={(event) => handleFilterChange(event, "kunjungan")}
+              value={filter.kunjungan}
+              type="date"
+              name=""
+              id=""
+              className={`px-2 py-3 bg-lightGrey rounded-sm placeholder:text-[12px] flex-1 outline-none text-[12px]`}
+            />
           </div>
 
-          <div className='w-[20%] flex flex-col gap-y-1'>
-            <label htmlFor="" className='text-[14px] font-medium'>Gol Darah</label>
-            <select onChange={(event) => handleFilterChange(event, 'golonganDarah')} name="" id="" className={`px-2 py-3 bg-lightGrey rounded-sm placeholder:text-[12px] flex-1 outline-none text-[12px]`}>
-              <option value="">All</option>
-              <option value="">A</option>
-              <option value="">A+</option>
-              <option value="">A-</option>
-              <option value="">B</option>
-              <option value="">B+</option>
-              <option value="">B-</option>
-              <option value="">AB</option>
-              <option value="">AB+</option>
-              <option value="">AB-</option>
-              <option value="">O</option>
+          <div className="w-[20%] flex flex-col gap-y-1">
+            <label htmlFor="" className="text-[14px] font-medium">
+              Gol Darah
+            </label>
+            <select
+              onChange={(event) => handleFilterChange(event, "golonganDarah")}
+              value={filter.golonganDarah}
+              name=""
+              id=""
+              className={`px-2 py-3 bg-lightGrey rounded-sm placeholder:text-[12px] flex-1 outline-none text-[12px]`}
+            >
+              <option value="">Semua</option>
+              <option value="A">A</option>
+              <option value="A+">A+</option>
+              <option value="A-">A-</option>
+              <option value="B">B</option>
+              <option value="B+">B+</option>
+              <option value="B-">B-</option>
+              <option value="AB">AB</option>
+              <option value="B+">AB+</option>
+              <option value="AB-">AB-</option>
+              <option value="O">O</option>
             </select>
           </div>
         </div>
-
       </article>
-
 
       <article className="w-full bg-white rounded-lg shadow-lg px-4 py-6 mt-6">
+        {/* title and info */}
+        <div className="w-full flex justify-between items-center">
+          <h3 className="text-xl font-medium tracking-wide">Data Pasien</h3>
+          <h5 className="text-sm tracking-wide text-greySecondary flex gap-x-4 items-center justify-center">
+            <button
+              onClick={() =>
+                handlePageChange(
+                  data.currentPage === 1
+                    ? data.currentPage
+                    : data.currentPage - 1
+                )
+              }
+            >
+              <ChevronLeft
+                size={20}
+                className={`${
+                  data.currentPage === 1
+                    ? "stroke-greyPrimary"
+                    : "hover:stroke-blue-500 duration-200 ease-in-out"
+                }`}
+              />
+            </button>
+            <span className="text-blue-500">{data.currentPage}</span> of{" "}
+            <span className="text-blue-500">{data.numOfPages}</span>
+            <button
+              onClick={() =>
+                handlePageChange(
+                  data.currentPage === data.numOfPages
+                    ? data.currentPage
+                    : data.currentPage + 1
+                )
+              }
+              disabled={data.currentPage === data.numOfPages}
+            >
+              <ChevronRight
+                size={20}
+                className={`${
+                  data.currentPage === data.numOfPages
+                    ? "stroke-greyPrimary"
+                    : "hover:stroke-blue-500 duration-200 ease-in-out"
+                }`}
+              />
+            </button>
+          </h5>
+        </div>
 
-          {/* title and info */}
-          <div className='w-full flex justify-between items-center'>
-            <h3 className='text-xl font-medium tracking-wide'>Data Pasien</h3>
-            <h5 className='text-sm tracking-wide text-greySecondary flex gap-x-4 items-center justify-center'>
+        {/* table header */}
+        <div className="w-full flex items-center gap-x-2 mt-8">
+          <p className="text-sm font-medium w-[15%]">Rekam medis</p>
+          <p className="text-sm font-medium w-[15%]">Nama</p>
+          <p className="text-sm font-medium w-[10%]">Gender</p>
+          <p className="text-sm font-medium w-[12%]">Tujuan</p>
+          <p className="text-sm font-medium w-[10%] truncate text-center">
+            Resiko
+          </p>
+          <p className="text-sm font-medium w-[15%] text-center">NIK</p>
+          <p className="text-sm font-medium w-[15%] text-center">Registrasi</p>
+          <p className="text-sm font-medium w-[13%] text-center">Status</p>
+        </div>
 
-              <Link to={`?page=${data.currentPage === 1 ? data.currentPage.toString() : (data.currentPage - 1).toString()}`}>
-                <ChevronLeft size={20} className='hover:stroke-blue-500 duration-200 ease-in-out'/>
-              </Link>
-              <span className='text-blue-500'>{data.currentPage}</span> of <span className='text-blue-500'>{data.numOfPages}</span>
-              
-              <Link to={`?page=${data.currentPage === data.numOfPages ? data.currentPage.toString() : (data.currentPage + 1).toString()}`} className={data.currentPage === data.numOfPages ? 'invisible' : null}>
-                <ChevronRight size={20} className='hover:stroke-blue-500 duration-200 ease-in-out' />
-              </Link>
-            </h5>
-          </div>
-
-          {/* table header */}
-          <div className='w-full flex items-center gap-x-2 mt-8'>
-            <p className='text-sm font-medium w-[15%]'>Rekam medis</p>
-            <p className='text-sm font-medium w-[15%]'>Nama</p>
-            <p className='text-sm font-medium w-[10%]'>Gender</p>
-            <p className='text-sm font-medium w-[12%]'>Tujuan</p>
-            <p className='text-sm font-medium w-[10%] truncate text-center'>Resiko</p>
-            <p className='text-sm font-medium w-[15%] text-center'>NIK</p>
-            <p className='text-sm font-medium w-[15%] text-center'>Registrasi</p>
-            <p className='text-sm font-medium w-[13%] text-center'>Status</p>
-          </div>
-
-          {/* isi data */}
-          <div className='w-full mt-8 flex flex-col gap-y-1'>
-            {data.pasien.map((item, index) => {
-              return (
-                <TableData key={index} {...item} />
-              )
-            })}
-          </div>
+        {/* isi data */}
+        <div className="w-full mt-8 flex flex-col gap-y-1">
+          {data.pasien.map((item, index) => {
+            return <TableData key={index} {...item} />;
+          })}
+        </div>
       </article>
-
     </section>
-  )
-}
+  );
+};
 
-export default AllData
+export default AllData;
--- controllers/rekamMedisController.js
+++ controllers/rekamMedisController.js
@@ -1,73 +1,82 @@
-import { StatusCodes } from 'http-status-codes';
-import RekamMedis from '../models/PasienModel.js'
+import { StatusCodes } from "http-status-codes";
+import RekamMedis from "../models/PasienModel.js";
 
-export const statsData = async(req, res) => {
-    const totalPasien = await RekamMedis.countDocuments()
-    const totalResiko = await RekamMedis.aggregate([
-        {
-            $group: {
-                _id: "$statusResiko",
-                count: { $sum: 1 }
-            }
-        }
-    ])
+export const statsData = async (req, res) => {
+  const totalPasien = await RekamMedis.countDocuments();
+  const totalResiko = await RekamMedis.aggregate([
+    {
+      $group: {
+        _id: "$statusResiko",
+        count: { $sum: 1 },
+      },
+    },
+  ]);
 
-    const rasioGender = await RekamMedis.aggregate([
-        {
-            $group: {
-                _id: '$jenisKelamin',
-                count: { $sum: 1 }
-            }
-        }
-    ])
-    const pasienTerbaru = await RekamMedis.find({}).sort({ createdAt: -1 })
-    
-    
+  const rasioGender = await RekamMedis.aggregate([
+    {
+      $group: {
+        _id: "$jenisKelamin",
+        count: { $sum: 1 },
+      },
+    },
+  ]);
+  const pasienTerbaru = await RekamMedis.find({}).sort({ createdAt: -1 });
 
-    return res.status(StatusCodes.OK).json({ totalPasien, totalResiko, rasioGender, pasienTerbaru })
-}
+  return res
+    .status(StatusCodes.OK)
+    .json({ totalPasien, totalResiko, rasioGender, pasienTerbaru });
+};
 
-export const createData = async(req, res) => {
-    req.body.createdBy = req.user.userId;
-    await RekamMedis.create(req.body)
-    return res.status(StatusCodes.OK).json({ msg: 'pasien di input' })
-}
+export const createData = async (req, res) => {
+  req.body.createdBy = req.user.userId;
+  await RekamMedis.create(req.body);
+  return res.status(StatusCodes.OK).json({ msg: "pasien di input" });
+};
 
-export const getAllData = async(req, res) => {
+export const getAllData = async (req, res) => {
+  console.log(req.query);
 
-    const page = Number(req.query.page) || 1;
-    const limit = 10;
-    const skip = (page - 1) * limit;
+  const page = Number(req.query.page) || 1;
+  const limit = 10;
+  const skip = (page - 1) * limit;
 
-    const data = await RekamMedis.find({}).skip(skip).limit(limit)
-    const countDocument = await RekamMedis.countDocuments()
+  const data = await RekamMedis.find({}).skip(skip).limit(limit);
+  const countDocument = await RekamMedis.countDocuments();
 
-    const numOfPages = Math.ceil(countDocument / limit)
+  const numOfPages = Math.ceil(countDocument / limit);
 
-    return res.status(StatusCodes.OK).json({pasien: data, currentPage: page, numOfPages})
-}
+  return res
+    .status(StatusCodes.OK)
+    .json({ pasien: data, currentPage: page, numOfPages });
+};
 
-export const getSingleData = async(req, res) => {
-    const rekamMedis = await RekamMedis.findOne({ _id: req.params.id })
+export const getSingleData = async (req, res) => {
+  const rekamMedis = await RekamMedis.findOne({ _id: req.params.id });
 
-    return res.status(StatusCodes.OK).json({rekamMedis})
-}
+  return res.status(StatusCodes.OK).json({ rekamMedis });
+};
 
-export const updateData = async(req, res) => {
-    const {id} = req.params
-    await RekamMedis.findOneAndUpdate({_id: id}, req.body, {new: true, runValidators: true})
-    return res.status(StatusCodes.OK).json({ msg: 'Berhasil diupdate' })
-    
-}
+export const updateData = async (req, res) => {
+  const { id } = req.params;
+  await RekamMedis.findOneAndUpdate({ _id: id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  return res.status(StatusCodes.OK).json({ msg: "Berhasil diupdate" });
+};
 
-export const deleteData = async(req, res) => {
-    const { id } = req.params
-    await RekamMedis.findOneAndDelete({_id: id})
-    return res.status(StatusCodes.OK).json({ msg: 'user deleted!' })
-}
+export const deleteData = async (req, res) => {
+  const { id } = req.params;
+  await RekamMedis.findOneAndDelete({ _id: id });
+  return res.status(StatusCodes.OK).json({ msg: "user deleted!" });
+};
 
-export const addKunjungan = async(req, res) => {
-    const { id } = req.params
-    const user = await RekamMedis.findOneAndUpdate({_id: id}, { $push: { riwayatKunjungan: req.body }}, {new: true, runValidators: true} )
-    return res.status(StatusCodes.OK).json({ msg: 'Okay' })
-}
+export const addKunjungan = async (req, res) => {
+  const { id } = req.params;
+  const user = await RekamMedis.findOneAndUpdate(
+    { _id: id },
+    { $push: { riwayatKunjungan: req.body } },
+    { new: true, runValidators: true }
+  );
+  return res.status(StatusCodes.OK).json({ msg: "Okay" });
+};
