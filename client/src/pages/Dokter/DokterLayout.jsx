1cdca4ae259a3d33f089d88ac93b21c3d91c2825
Aan Bahudin <aan.bahudin11@gmail.com>
2025-01-22 23:38:05 +0800

    custom hook context setup

--- client/src/pages/Dokter/DokterLayout.jsx
+++ client/src/pages/Dokter/DokterLayout.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { createContext, useContext } from 'react'
+import { useLoaderData } from 'react-router'
+
+// this loader only to check if user is logged in or they authorized to access this route
+export const loader = async() => {
+  return null
+}
+
+const DokterContext = createContext()
 
 const DokterLayout = () => {
   return (
-    <div>DokterLayout</div>
+    <DokterContext.Provider value={{}}>
+      <div>DokterLayout</div>
+    </DokterContext.Provider>
   )
 }
 
+export const useDokterContext = () => useContext(DokterContext)
 export default DokterLayout
