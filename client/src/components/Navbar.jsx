256fdfde74aeb1b8e7ad92a4d961b0e1f51ac8ef
Aan Bahudin <aan.bahudin11@gmail.com>
2025-01-22 22:33:47 +0800

    navbar background page fix

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,11 +6,11 @@ const Navbar = () => {
     const { user, toggleSidebar } = useAdminContext()
 
   return (
-    <div className='w-full h-[70px] flex items-center justify-between bg-slate-100  px-10'>
-        <Menu size={30} onClick={toggleSidebar} />
+    <div className='w-full h-[70px] flex items-center justify-between bg-slate-50  px-10'>
+        <Menu className='stoke-slate-500 ' onClick={toggleSidebar} />
 
         <section className='flex items-center justify-center gap-x-2 my-auto'>
-            <User className='my-auto' size={20} />
+            <User className='my-auto stroke-slate-600' size={20} />
             <h5 className='text-sm my-auto text-center'>{user.nama}</h5>
         </section>
     </div> 
