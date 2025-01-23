f4f61e88352ee920a154816fc5e816ae399b6aff
Aan Bahudin <aan.bahudin11@gmail.com>
2025-01-23 13:37:04 +0800

    basic CRUD in dokter controller

--- controllers/Admin/dokterController.js
+++ controllers/Admin/dokterController.js
@@ -1,21 +1,29 @@
 import { StatusCodes } from "http-status-codes"
+import Dokter from '../../models/DokterModel.js'
 
 export const getAllDokter = async(req, res) => {
-    return res.status(StatusCodes.OK).json({msg: 'success'})
+    const dokter = await Dokter.find()
+    return res.status(StatusCodes.OK).json({msg: 'success', dokter, total: dokter.length})
 }
 
 export const addDokter = async(req, res) => {
-    return res.status(StatusCodes.OK).json({msg: 'ok'})
+    const dokter = await Dokter.create(req.body)
+    return res.status(StatusCodes.CREATED).json({msg: 'success', data: dokter})
 }
 
 export const getSingleDokter = async(req, res) => {
-    return res.status(StatusCodes.OK).json({msg: 'ok'})
+    const dokter = await Dokter.findOne({_id: req.params.id})
+    return res.status(StatusCodes.OK).json({msg: 'ok', dokter})
 }
 
 export const updateDokter = async(req, res) => {
-    return res.status(StatusCodes.OK).json({msg: 'ok'})
+    const {id} = req.params
+    const updatedDokter = await Dokter.findOneAndUpdate({_id: id}, req.body, {new: true, runValidators: true})
+    return res.status(StatusCodes.OK).json({msg: 'ok', updateDokter})
 }
 
 export const deleteDokter = async(req, res) => {
-    return res.status(StatusCodes.OK).json({msg: 'ok'})
+    const {id} = req.params
+    await Dokter.findOneAndDelete({_id: id})
+    return res.status(StatusCodes.OK).json({msg: 'successfully deleted'})
 }
