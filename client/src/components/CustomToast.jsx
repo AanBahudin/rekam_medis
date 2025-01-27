0f59b9befe7d34be5048df11b0c0e486bc64efa0
Aan Bahudin <aan.bahudin11@gmail.com>
2025-01-27 14:25:05 +0800

    custom toast small change

--- client/src/components/CustomToast.jsx
+++ client/src/components/CustomToast.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { Check } from 'lucide-react'
+import { Check, CircleAlert } from 'lucide-react'
 
 const CustomToast = ({type, title, description}) => {
   return (
     <div className='w-full flex items-center gap-x-4'>
-        <Check className='w-6 h-6 stroke-[#4BB543]' />
+
+        {type === 1 ? <Check className='w-6 h-6 stroke-[#4BB543]' /> : (
+          type === 2 ? <CircleAlert className='w-6 h-6 stroke-[#FFCC00]' /> : <Ban className='w-6 h-6 stroke-[#CC3300]' />
+        )}
+        
 
         <div className='flex-1'>
             <h1 className='font-semibold text-slate-700'>{title}</h1>
